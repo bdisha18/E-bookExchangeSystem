d1792bdeef8f6c6e3b0d4e2f9468f8ab572d749c
RakshaJain <jainraksha17@gmail.com>
2019-03-11 16:54:51 +0530

    message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in!<a href="{{route('admin.dashboard')}}" style="text-decoration: none">   Head to Profile</a>
                 </div>
             </div>
         </div>
